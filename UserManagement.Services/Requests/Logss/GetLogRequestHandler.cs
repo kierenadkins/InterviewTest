17802e4af83ddf79478385a2812bb944cbae18c8
kierenadkins <75570969+kierenadkins@users.noreply.github.com>
2025-07-11 13:26:43 +0100

    add logs

---
+++ UserManagement.Services/Requests/Logss/GetLogRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using UserManagement.Models;
+using UserManagement.Services.Domain.Interfaces;
+
+namespace UserManagement.Application.Requests.UserR
+{
+    public class GetLogRequest : IRequest<List<Log>>
+    {
+    }
+
+    public class GetLogRequestHandler : IRequestHandler<GetLogRequest, List<Log>>
+    {
+        private readonly ILogService _logService;
+
+        public GetLogRequestHandler(ILogService logService)
+        {
+            _logService = logService;
+        }
+
+        public Task<List<Log>> Handle(GetLogRequest request, CancellationToken cancellationToken)
+        {
+            var logs = _logService.GetAll();
+            return Task.FromResult(logs.ToList());
+        }
+    }
+}
---
+++ UserManagement.Web/Controllers/LogController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using UserManagement.Application.Requests.UserR;
+using UserManagement.Web.Models.Users;
+
+namespace UserManagement.WebMS.Controllers;
+
+[Route("Logs")]
+public class LogController : Controller
+{
+    private ISender _sender;
+    public LogController(ISender sender) => _sender = sender;
+
+    [HttpGet]
+    public async Task<ViewResult> List()
+    {
+        var response = await _sender.Send(new GetLogRequest());
+        var model = response.Select(p => new LogUserModel
+        {
+            UserId = (int) p.UserId,
+            Action = p.Action,
+            Timestamp = p.Timestamp
+        }).ToList();
+
+        return View(model);
+    }
+}
+
